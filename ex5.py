95500035f912d166273c4cf4e65d85549cca4e64
foursking <lyf021408@gmail.com>
2013-06-21 18:56:51 +0800

    first dayt

---
+++ ex5.py
@@ -0,0 +1,16 @@
+#Here's some new strange stuff , remember type it exactly
+
+days = "Mon Tus Wed Thu Fri Sat Sun"
+months ="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: " , days
+print "Here are the months: %r "  % months
+
+print """
+
+There's somethint going on here.
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 line if we want , or 5 , or 6.
+"""
+
